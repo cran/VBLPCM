1aff77403368e27bea59512b3b0060608a9a82d5
Michael Salter-Townshend <michael.salter-townshend@ucd.ie>
2012-08-17 00:00:00 +0000

    version 2.2

--- R/zzz.R
+++ R/zzz.R
@@ -1,17 +1,11 @@
-.First.lib <- function(lib,pkg) {
+.onAttach <- function(lib,pkg) {
     library.dynam("VBLPCM", pkg, lib)
-
-    ehelp <- library(help="VBLPCM",lib.loc=lib,character.only=TRUE)[["info"]][[1]]
+    ehelp <- packageDescription("VBLPCM")
     packageStartupMessage("\n\n")
-    packageStartupMessage(paste(substring(ehelp[4],first=16),"\n",
-              "Version ",substring(ehelp[2],first=16),
-              " created on ",
-              substring(ehelp[3],first=16),".\n", sep=""))
-
+    packageStartupMessage(paste("\n",ehelp[3],"\n"),paste(ehelp[1], "Version", ehelp[4], 
+        "Created on", ehelp[5]), paste("\n Created and maintained by ", ehelp[6]), "\n")
     packageStartupMessage('For citation information type \'citation("VBLPCM")\'\n')
     packageStartupMessage('Type \'help(VBLPCM)\' to get started.\n')
     packageStartupMessage('Some worked examples are given by \'example(VBLPCM)\' \n')
 } 
 
-
-.onLoad <- function(lib, pkg) {library.dynam("VBLPCM", pkg, lib)}
